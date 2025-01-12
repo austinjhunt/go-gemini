a92ecb9edd8baf6beb98c31d273d378aaeb0b77e
Austin Hunt <austinsketches@gmail.com>
2025-01-12 17:14:13 -0500

    add public API util methods for getting coin price, converting USD to a crypto coin amount; use new Ticker types for GetTicker method returns; move GetPublicEndpoint and DownloadPublicFile into public.go

--- public/public.go
+++ public/public.go
@@ -1,7 +1,12 @@
 package public
 
 import (
+	"encoding/json"
+	"errors"
+	"io"
 	"log"
+	"net/http"
+	"os"
 	"strconv"
 
 	"github.com/austinjhunt/go-gemini/util"
@@ -9,20 +14,109 @@ import (
 
 // invoke Gemini exchange REST API public endpoints; one function per endpoint
 
+func GetPublicEndpoint(endpoint string, target interface{}) error {
+	/*
+		Perform an HTTP GET request on a public Gemini API endpoint and unmarshal the JSON response into the provided target interface.
+
+		Args:
+		endpoint (string) - API endpoint to use
+		target - any type of JSON object in which the JSON response gets stored, passed as &target (pointer to a variable in which response is to be stored) when method is invoked
+
+		Returns nothing if successful, returns error if it fails
+
+	*/
+
+	url := util.GetBaseAPIUrl() + endpoint
+
+	method := "GET"
+
+	client := &http.Client{}
+	req, err := http.NewRequest(method, url, nil)
+	if err != nil {
+		return errors.New("error creating request: " + err.Error())
+	}
+
+	req.Header.Add("Content-Type", "application/json")
+
+	res, err := client.Do(req)
+	if err != nil {
+		return errors.New("error making request: " + err.Error())
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		return errors.New("received non-200 status code: " + res.Status)
+	}
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return errors.New("error reading response body: " + err.Error())
+	}
+
+	err = json.Unmarshal(body, target)
+	if err != nil {
+		return errors.New("error unmarshalling response: " + err.Error())
+	}
+
+	return nil
+}
+
+func DownloadPublicFile(endpoint string, filePath string) error {
+	/*
+		Download a file from a url to a specific file path
+
+		Args:
+			endpoint (string): endpoint from which to download file
+			filePath (string): path on local file system to which downloaded file will be saved
+
+		Returns:
+			error: Returns an error if the file cannot be downloaded.
+	*/
+
+	// Perform the HTTP GET request to download the file.
+	url := util.GetBaseAPIUrl() + endpoint
+	response, err := http.Get(url)
+	if err != nil {
+		return errors.New("failed to download funding amount report: " + err.Error())
+	}
+	defer response.Body.Close()
+
+	// Check if the response status is OK.
+	if response.StatusCode != http.StatusOK {
+		return errors.New("unexpected HTTP status: " + response.Status)
+	}
+
+	// Create the file locally.
+	file, err := os.Create(filePath)
+	if err != nil {
+		return errors.New("failed to create the file: " + err.Error())
+	}
+	defer file.Close()
+
+	// Copy the response body to the file.
+	_, err = io.Copy(file, response.Body)
+	if err != nil {
+		return errors.New("failed to save the report: " + err.Error())
+	}
+
+	log.Printf("Report downloaded successfully and saved to %s\n", filePath)
+	return nil
+}
+
 func GetSymbols() []string {
-	/* 
-	Get a list of all available symbols
+	/*
+		Get a list of all available symbols
 
-	Args:
-	None
+		Args:
+		None
 
-	Returns:
-	The response will be an array of strings
+		Returns:
+		The response will be an array of strings
 	*/
 	var symbols []string
 	url := "/v1/symbols"
 
-	err := util.GetPublicEndpoint(url, &symbols)
+	err := GetPublicEndpoint(url, &symbols)
 	if err != nil {
 		log.Fatalf("Error fetching symbols: %v", err)
 	}
@@ -32,17 +126,17 @@ func GetSymbols() []string {
 
 func GetSymbolDetails(symbol string) map[string]interface{} {
 	/*
-	Get extra detail on supported symbols, such as minimum order size, tick size, quote increment and more
+		Get extra detail on supported symbols, such as minimum order size, tick size, quote increment and more
 
-	Args:
-	symbol (string): Trading pair symbol. See symbols and minimums
-	
+		Args:
+		symbol (string): Trading pair symbol. See symbols and minimums
+
 	*/
 
 	var details map[string]interface{}
 	url := "/v1/symbols/details/" + symbol
 
-	err := util.GetPublicEndpoint(url, &details)
+	err := GetPublicEndpoint(url, &details)
 	if err != nil {
 		log.Fatalf("Error fetching symbol details: %v", err)
 	}
@@ -54,7 +148,7 @@ func GetNetwork(token string) map[string]interface{} {
 	var network map[string]interface{}
 	url := "/v1/network/" + token
 
-	err := util.GetPublicEndpoint(url, &network)
+	err := GetPublicEndpoint(url, &network)
 	if err != nil {
 		log.Fatalf("Error fetching network: %v", err)
 	}
@@ -62,35 +156,37 @@ func GetNetwork(token string) map[string]interface{} {
 	return network
 }
 
-func GetTicker(symbol string) map[string]interface{} {
-	var ticker map[string]interface{}
+func GetTicker(symbol string) *TickerV1 {
+	var ticker TickerV1
 	url := "/v1/pubticker/" + symbol
 
-	err := util.GetPublicEndpoint(url, &ticker)
+	err := GetPublicEndpoint(url, &ticker)
 	if err != nil {
 		log.Fatalf("Error fetching ticker: %v", err)
+		return nil
 	}
 
-	return ticker
+	return &ticker
 }
 
-func GetTickerV2(symbol string) map[string]interface{} {
-	var ticker map[string]interface{}
-	url := "https://api.gemini.com/v2/ticker/" + symbol
+func GetTickerV2(symbol string) *TickerV2 {
+	var ticker TickerV2
+	url := "/v2/ticker/" + symbol
 
-	err := util.GetPublicEndpoint(url, &ticker)
+	err := GetPublicEndpoint(url, &ticker)
 	if err != nil {
 		log.Fatalf("Error fetching ticker v2: %v", err)
+		return nil
 	}
 
-	return ticker
+	return &ticker
 }
 
 func GetCandles(symbol string, time_frame string) [][]interface{} {
 	var candles [][]interface{}
-	url := "https://api.gemini.com/v2/candles/" + symbol + "/" + time_frame
+	url := "/v2/candles/" + symbol + "/" + time_frame
 
-	err := util.GetPublicEndpoint(url, &candles)
+	err := GetPublicEndpoint(url, &candles)
 	if err != nil {
 		log.Fatalf("Error fetching candles: %v", err)
 	}
@@ -98,23 +194,22 @@ func GetCandles(symbol string, time_frame string) [][]interface{} {
 	return candles
 }
 
-
 func GetDerivativesCandles(symbol string, time_frame string) [][]interface{} {
-	/* 
-	Get time-intervaled data for the provided perps symbol
+	/*
+		Get time-intervaled data for the provided perps symbol
 
-	Args: 
-	symbol (string): Trading pair symbol. Available only for perpetual pairs like BTCGUSDPERP, See symbols and minimums
-	time_frame (string): Time range for each candle. 1m: 1 minute (only)
+		Args:
+		symbol (string): Trading pair symbol. Available only for perpetual pairs like BTCGUSDPERP, See symbols and minimums
+		time_frame (string): Time range for each candle. 1m: 1 minute (only)
 
-	Returns:
-	The response will be an array of arrays
+		Returns:
+		The response will be an array of arrays
 	*/
 	var derivativesCandles [][]interface{}
-	 
-	url := "https://api.gemini.com/v2/derivatives/candles/" + symbol + "/" + time_frame
 
-	err := util.GetPublicEndpoint(url, &derivativesCandles)
+	url := "/v2/derivatives/candles/" + symbol + "/" + time_frame
+
+	err := GetPublicEndpoint(url, &derivativesCandles)
 
 	if err != nil {
 		log.Fatalf("Error fetching Derivatives Candles %v", err)
@@ -123,20 +218,20 @@ func GetDerivativesCandles(symbol string, time_frame string) [][]interface{} {
 }
 
 func GetFeePromos() map[string]interface{} {
-	/* 
-	Get symbols that currently have fee promos
+	/*
+		Get symbols that currently have fee promos
 
-	Args: 
-	None
+		Args:
+		None
 
-	Returns:
-	The response will be a JSON object
+		Returns:
+		The response will be a JSON object
 	*/
-	 
+
 	var feePromos map[string]interface{}
 
 	url := "/v1/feepromos"
-	err := util.GetPublicEndpoint(url, &feePromos) 
+	err := GetPublicEndpoint(url, &feePromos)
 	if err != nil {
 		log.Fatalf("Error fetching fee promos %v", err)
 	}
@@ -144,44 +239,44 @@ func GetFeePromos() map[string]interface{} {
 }
 
 func GetCurrentOrderBook(symbol string) map[string]interface{} {
-	/* 
-	Return the current order book as two arrays (bids / asks)
+	/*
+		Return the current order book as two arrays (bids / asks)
 
-	Args: 
-	symbol (string): Trading pair symbol. See symbols and minimums
+		Args:
+		symbol (string): Trading pair symbol. See symbols and minimums
 
-	Returns:
-	The response will be two arrays
+		Returns:
+		The response will be two arrays
 	*/
-	 
+
 	var currentOrderBook map[string]interface{}
 
 	url := "/v1/book/" + symbol
 
-	err := util.GetPublicEndpoint(url, &currentOrderBook)
+	err := GetPublicEndpoint(url, &currentOrderBook)
 
 	if err != nil {
 		log.Fatalf("Error fetching Current Order Book %v", err)
 	}
-	
+
 	return currentOrderBook
 }
 
 func GetTradeHistory(symbol string) []map[string]interface{} {
-	/* 
-	Return the trades that have executed since the specified timestamp
+	/*
+		Return the trades that have executed since the specified timestamp
 
-	Args: 
-	symbol (string): Trading pair symbol. See symbols and minimums
+		Args:
+		symbol (string): Trading pair symbol. See symbols and minimums
 
-	Returns:
-	The response will be an array of JSON objects, sorted by timestamp, with the newest trade shown first
+		Returns:
+		The response will be an array of JSON objects, sorted by timestamp, with the newest trade shown first
 	*/
 	var tradeHistory []map[string]interface{}
-	 
+
 	url := "/v1/trades/" + symbol
-	err := util.GetPublicEndpoint(url, &tradeHistory)
-	
+	err := GetPublicEndpoint(url, &tradeHistory)
+
 	if err != nil {
 		log.Fatalf("Error fetching Trade History %v", err)
 	}
@@ -189,19 +284,19 @@ func GetTradeHistory(symbol string) []map[string]interface{} {
 }
 
 func GetPriceFeed() []map[string]interface{} {
-	/* 
-	Return a list of objects, one for each pair, with the current price and 24 hour change in price
+	/*
+		Return a list of objects, one for each pair, with the current price and 24 hour change in price
 
-	Args: 
-	None
+		Args:
+		None
 
-	Returns:
-	Response is a list of objects, one for each pair, with the following fields
+		Returns:
+		Response is a list of objects, one for each pair, with the following fields
 	*/
 	var priceFeed []map[string]interface{}
-	
+
 	url := "/v1/pricefeed"
-	err := util.GetPublicEndpoint(url, &priceFeed)
+	err := GetPublicEndpoint(url, &priceFeed)
 	if err != nil {
 		log.Fatalf("Error fetching price feed %v", err)
 	}
@@ -209,19 +304,19 @@ func GetPriceFeed() []map[string]interface{} {
 }
 
 func GetFundingAmount(symbol string) map[string]interface{} {
-	/* 
-	Get extra detail on supported symbols, such as minimum order size, tick size, quote increment and more
+	/*
+		Get extra detail on supported symbols, such as minimum order size, tick size, quote increment and more
 
-	Args: 
-	symbol (string): Trading pair symbol. See symbols and minimums
+		Args:
+		symbol (string): Trading pair symbol. See symbols and minimums
 
-	Returns:
-	The response will be an object
+		Returns:
+		The response will be an object
 	*/
 	var fundingAmount map[string]interface{}
 	url := "/v1/fundingamount/" + symbol
 
-	err := util.GetPublicEndpoint(url, &fundingAmount) 
+	err := GetPublicEndpoint(url, &fundingAmount)
 	if err != nil {
 		log.Fatalf("Error fetching funding amount %v", err)
 	}
@@ -229,28 +324,61 @@ func GetFundingAmount(symbol string) map[string]interface{} {
 }
 func DownloadFundingAmountReport(symbol string, fromDate string, toDate string, numRows int) error {
 	/*
-	Downloads a CSV or Excel file with funding amount records.
+		Downloads a CSV or Excel file with funding amount records.
 
-	Args: 
-		symbol (string): Trading pair symbol. See symbols and minimums.
-		fromDate (string): Start date for the report (format: yyyy-MM-dd).
-		toDate (string): End date for the report (format: yyyy-MM-dd).
-		numRows (int): Maximum number of records to fetch.
+		Args:
+			symbol (string): Trading pair symbol. See symbols and minimums.
+			fromDate (string): Start date for the report (format: yyyy-MM-dd).
+			toDate (string): End date for the report (format: yyyy-MM-dd).
+			numRows (int): Maximum number of records to fetch.
 
-	Returns:
-		error: Returns an error if the file cannot be downloaded.
+		Returns:
+			error: Returns an error if the file cannot be downloaded.
 	*/
 
 	// Construct the URL for the report.
 	url := "/v1/fundingamountreport/records.xlsx?symbol=" + symbol + "&fromDate=" + fromDate + "&toDate=" + toDate + "&numRows=" + strconv.Itoa(numRows)
-	
+
 	// Define the file path to save the downloaded file.
 	filePath := "funding_amount_report_" + symbol + "_" + fromDate + "_to_" + toDate + ".xlsx"
 
-	err := util.DownloadPublicFile(url, filePath)
+	err := DownloadPublicFile(url, filePath)
 	if err != nil {
 		log.Fatalf("%s", err.Error())
 	}
 	return nil
 }
 
+func GetCurrentCoinPriceUSD(symbol string) float64 {
+	ticker := GetTickerV2(symbol)
+	if ticker == nil {
+		log.Fatalf("Ticker data for %s not found", symbol)
+		return -1
+	}
+
+	askPrice, err := strconv.ParseFloat(ticker.Ask, 64)
+	if err != nil {
+		log.Fatalf("Invalid ask price for %s: %v", symbol, askPrice)
+		return -1
+	}
+
+	return askPrice
+}
+
+func ConvertUSDToCryptoAmount(dollarAmount float64, symbol string) float64 {
+	ticker := GetTickerV2(symbol)
+	if ticker == nil {
+		log.Fatalf("Ticker data for %s not found", symbol)
+		return -1
+	}
+
+	askPrice, err := strconv.ParseFloat(ticker.Ask, 64)
+	if err != nil {
+		log.Fatalf("Invalid ask price for %s: %v", symbol, askPrice)
+		return -1
+	}
+
+	cryptoAmount := dollarAmount / askPrice
+
+	return cryptoAmount
+}
